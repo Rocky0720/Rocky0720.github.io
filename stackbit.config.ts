351bc8000984f51cd74eab7a0cab77532ae3241d
Rocky0720 <121787671+Rocky0720@users.noreply.github.com>
2025-02-27 23:21:13 +0800

    Create stackbit.config.ts

---
+++ stackbit.config.ts
@@ -0,0 +1,22 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ],
+  modelExtensions: [{ name: "page", type: "page", urlPath: "/{slug}" }]
+});
